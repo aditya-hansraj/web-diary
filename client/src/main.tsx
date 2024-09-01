8a0f28d40832cc4434be9019789a474483412f37
aditya-hansraj <04adityahansraj@gmail.com>
2024-09-01 08:01:11 +0530

    change

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,7 +4,6 @@ import App from "./App.tsx";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { AuthProvider } from "./contexts/auth.context.tsx";
-import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
